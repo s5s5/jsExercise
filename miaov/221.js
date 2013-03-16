c89edfe8f4d22feba8638676ac195b0261357c19
s5s5 <s5s5cn@gmail.com>
2013-03-17 02:58:41 +0800

    miaov js exercise 221

--- miaov/221.js
+++ miaov/221.js
@@ -8,12 +8,42 @@ function getStyle(obj, attr) {
 }
 
 function startMove(obj, json, fn) {
-	var bStop = true; //运动是否值
-	for (var attr in json) {
-		//1.取当前的值
-		var iCur = 0;
-		if (attr === 'opacity') {
-			iCur = parseInt(parseFloat(getStyle(obj, attr)))
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var bStop = true; //运动是否值
+		for (var attr in json) {
+			//1.取当前的值
+			var iCur = 0;
+			if (attr === 'opacity') {
+				iCur = parseInt(parseFloat(getStyle(obj, attr)) * 100);
+			}
+			else {
+				iCur = parseInt(getStyle(obj, attr));
+			}
+
+			//2.算速度
+			var iSpeed = (json[attr] - iCur) / 8;
+			iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+
+			//3.检测停止
+			if (iCur !== json[attr]) {
+				bStop = false;
+			}
+
+			if (attr === 'opacity') {
+				obj.style.filter = 'alpha(opacity:' + (iCur + iSpeed) + ')';
+				obj.style.opacity = (iCur + iSpeed) / 100;
+			}
+			else {
+				obj.style[attr] = iCur + iSpeed + 'px';
+			}
+
+			if (bStop) {
+				clearInterval(obj.timer);
+				if (fn) {
+					fn();
+				}
+			}
 		}
-	}
+	}, 30);
 }
