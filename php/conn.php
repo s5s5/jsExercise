5e0b1beef7e60a67dc68d9b2562e904733641168
s5s5 <s5s5cn@gmail.com>
2013-12-21 15:36:16 +0800

    learn PHP

---
+++ php/conn.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<?
+@mysql_connect("localhost", "root", "root")or die("mysql 连接失败");
+@mysql_select_db("php100")or die("db连接失败");
+//    mysql_set_charset("utf8");
+mysql_query("set names 'utf8'");
+echo "php";
+?>
+<p>test</p>
---
+++ php/test.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="edge">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>learn PHP</title>
+</head>
+<body><?
+echo "PHP" . "<br>";
+$url = "s5s5.me";
+echo $url;
+unset($url);
+echo $url;
+echo __FILE__;
+echo __LINE__;
+define("NA", 123);
+echo NA;
+?>
+</body>
+</html>
