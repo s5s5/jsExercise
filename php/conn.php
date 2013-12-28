eb2139e4f2eea34b25ab075b38087d86b2a0dd42
s5s5 <s5s5cn@gmail.com>
2013-12-28 16:54:37 +0800

    learn PHP

--- php/conn.php
+++ php/conn.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
 <?
 @mysql_connect("localhost", "root", "root")or die("mysql 连接失败");
 @mysql_select_db("php100")or die("db连接失败");
-//    mysql_set_charset("utf8");
-mysql_query("set names 'utf8'");
-echo "php";
+//mysql_set_charset("utf8");
+mysql_query("set names utf8");
 ?>
-<p>test</p>
