51c448cc062083780ecf3eeb949d5e964f17513c
s5s5 <s5s5cn@gmail.com>
2013-12-31 18:03:08 +0800

    learn PHP

--- php/weibo/callback.php
+++ php/weibo/callback.php
@@ -1,14 +1,13 @@
 <?php
 
 session_start();
-include_once( 'config.php' );
-include_once( 'weibooauth.php' );
+include_once('config.php');
+include_once('weibooauth.php');
 
 
+$o = new WeiboOAuth(WB_AKEY, WB_SKEY, $_SESSION['keys']['oauth_token'], $_SESSION['keys']['oauth_token_secret']);
 
-$o = new WeiboOAuth( WB_AKEY , WB_SKEY , $_SESSION['keys']['oauth_token'] , $_SESSION['keys']['oauth_token_secret']  );
-
-$last_key = $o->getAccessToken(  $_REQUEST['oauth_verifier'] ) ;
+$last_key = $o->getAccessToken($_REQUEST['oauth_verifier']);
 
 $_SESSION['last_key'] = $last_key;
 
--- php/weibo/index.php
+++ php/weibo/index.php
@@ -2,27 +2,25 @@
 
 session_start();
 //if( isset($_SESSION['last_key']) ) header("Location: weibolist.php");
-include_once( 'config.php' );
-include_once( 'weibooauth.php' );
+include_once('config.php');
+include_once('weibooauth.php');
 
 
-
-$o = new WeiboOAuth( WB_AKEY , WB_SKEY  );
+$o = new WeiboOAuth(WB_AKEY, WB_SKEY);
 
 $keys = $o->getRequestToken();
 
 
+if (strpos('http://sona.misuisui.com/weibo/', 'index.php') === false)
+    $callback = 'http://sona.misuisui.com/weibo/callback.php';
+else
+    $callback = str_replace('index.php', 'callback.php', $_SERVER['SCRIPT_URI']);
 
-if( strpos( $_SERVER['SCRIPT_URI'] , 'index.php' ) === false )
-	$callback =  $_SERVER['SCRIPT_URI'].'/callback.php';
-else	
-	$callback =  str_replace( 'index.php' , 'callback.php' , $_SERVER['SCRIPT_URI'] );
 
-
-$aurl = $o->getAuthorizeURL( $keys['oauth_token'] ,false , $callback );
+$aurl = $o->getAuthorizeURL($keys['oauth_token'], false, $callback);
 
 $_SESSION['keys'] = $keys;
 
 
 ?>
-<a href="<?=$aurl?>">Use Oauth to login</a>
+<a href="<?php echo $aurl ?>">Use Oauth to login</a>
